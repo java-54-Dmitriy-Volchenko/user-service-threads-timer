62e1d7965d240b338ce0bbe6d741c98f294c765b
DmitriyVolchenko <dmitriy.volchenko@gmail.com>
2024-08-13 20:53:53 +0300

    HW-45 with input from console done

--- src/telran/multithreading/TimerControllerAppl.java
+++ src/telran/multithreading/TimerControllerAppl.java
@@ -1,21 +1,53 @@
 package telran.multithreading;
 
+import java.util.Scanner;
+
 public class TimerControllerAppl {
 
     public static void main(String[] args) throws InterruptedException {
-        Timer timer = new Timer("HH:mm:ss", 1000);
-        timer.start();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter time format (for example - HH:mm:ss): ");
+        String timeFormat = scanner.nextLine();
+        System.out.print("Enter timer resolution in milliseconds: ");
+        
+        Timer timer = null;
+        long resolutionMillis = 0;
+        
+        int counter = 3;
+        boolean validInput = false;
 
-        Thread.sleep(5000);
-        timer.interrupt();
+        while (counter > 0 && validInput != true) {
+            try {
+                resolutionMillis = Long.parseLong(scanner.nextLine());
+                timer = new Timer(timeFormat, resolutionMillis);
+                validInput = true;
+             
+            } catch (NumberFormatException e) {
+                System.out.println("Resolution must be a valid number. Try again!");
+                counter--;
+                if (counter == 0) {
+                    System.out.println("You have exhausted all attempts. By-by!!!");
+                    scanner.close();
+                    return;
+                }
+            }
+        }
 
-        String[] words = "I am working without timer!".split(" ");
+        if (validInput && timer != null) {
+            timer.start();
 
-     
+            Thread.sleep(5000);
+            timer.interrupt(); 
+        }
+
+        String[] words = "I am working without timer!!!".split(" ");
+
         for (String word : words) {
             System.out.printf("%s ", word);
-            Thread.sleep(1000);
+            Thread.sleep(1000); 
         }
+
+        scanner.close();
     }
-    
 }
+
